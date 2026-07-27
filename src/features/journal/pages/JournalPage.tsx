1baac38b43d38e9412c6b5d0d725aa45b6a5c7d4
legacycore2-lab <legacycore2@gmail.com>
2026-07-27 11:37:23 +0300

    style(journal): load Excel import trigger styles

--- src/features/journal/pages/JournalPage.tsx
+++ src/features/journal/pages/JournalPage.tsx
@@ -3,6 +3,7 @@ import { useJournal } from '../hooks/useJournal'
 import '../styles/journal.css'
 import '../styles/journal-entry.css'
 import '../styles/journal-import.css'
+import '../styles/journal-import-trigger.css'
 
 export function JournalPage() {
   const journal = useJournal()
